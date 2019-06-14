9743a6f25fc05cee0a80b4331e1e3f8eac9bf5e4
eckso <eckso@eckso.io>
2019-06-14 16:20:32 -0400

    improve setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,30 @@
 from setuptools import setup, find_packages
 
+name             = 'lura'
+version          = '0.2.0'
+author           = 'eckso'
+author_email     = 'eckso@eckso.io'
+url              = 'https://github.com/ecks0/lura'
+description      = 'syntactic sugar'
+long_description = open('README.md').read()
+long_description_content_type = 'text/markdown'
+python_requires  = ">= 3.6"
 install_requires = [
   'cryptography >= 2.7',
   'Jinja2 >= 2.10',
   'PyYAML >= 3.13',
-  #'msgpack >= 0.6.1',
 ]
 
 setup(
-  name = 'lura',
-  version = "0.1.0",
-  author = 'eckso',
-  author_email = 'eckso@eckso.io',
-  description = 'syntactic sugar',
-  packages = find_packages(),
-  python_requires = ">= 3.6",
+  name = name,
+  version = version,
+  author = author,
+  author_email = author_email,
+  description = description,
+  long_description = long_description,
+  long_description_content_type = long_description_content_type,
+  python_requires = python_requires,
   install_requires = install_requires,
+  packages = find_packages(),
   include_package_data = True,
 )
