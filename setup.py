be10bd0350bcbc3a3c34570a57f377c2e91f5e30
eckso <eckso@eckso.io>
2019-06-14 15:10:22 -0400

    remove unused entry_points arg

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ install_requires = [
   'cryptography >= 2.7',
   'Jinja2 >= 2.10',
   'PyYAML >= 3.13',
+  #'msgpack >= 0.6.1',
 ]
 
 setup(
@@ -16,5 +17,4 @@ setup(
   python_requires = ">= 3.6",
   install_requires = install_requires,
   include_package_data = True,
-  entry_points = entry_points,
 )
