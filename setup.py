acbcd7466714222a347a4875b5746e00c17ecfb6
eckso <eckso@eckso.io>
2019-06-16 19:52:17 -0400

    increment version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 name             = 'lura'
-version          = '0.3.1'
+version          = '0.3.2'
 author           = 'eckso'
 author_email     = 'eckso@eckso.io'
 url              = 'https://github.com/ecks0/lura'
