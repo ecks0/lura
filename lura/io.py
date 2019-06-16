7edc9148e760e5cdba98bd197771645a53f91d15
eckso <eckso@eckso.io>
2019-06-15 22:24:51 -0400

    add flush, tee, LineCallbackWriter, and LogWriter

--- lura/io.py
+++ lura/io.py
@@ -29,3 +29,70 @@ def fext(path):
   if '.' in path:
     return path.rsplit('.', 1)[1]
   raise ValueError(f'File has no extension: {path}')
+
+def flush(file):
+  if hasattr(file, 'flush') and callable(file.flush):
+    file.flush()
+
+def _tee(source, targets, cond):
+  while cond():
+    data = source.read()
+    if len(data) == 0:
+      break
+    for target in targets:
+      target.write(data)
+
+def tee(source, targets):
+  _tee(source, targets, cond=lambda: True)
+
+def Tee(source, targets, name=None):
+  from lura.threads import Thread
+  class _Tee(Thread):
+    def __init__(self):
+      super().__init__(target=self.work, name=name)
+      self.start()
+    def work(self):
+      self.work = True
+      _tee(source, targets, cond=lambda: self.work is True)
+    def stop(self):
+      self.work = False
+  thread = _Tee()
+  return thread
+
+class LineCallbackWriter:
+
+  def __init__(self):
+    super().__init__()
+    self.buf = []
+
+  def callback(self, lines):
+    for line in lines:
+      print(line)
+
+  def write(self, data):
+    if '\n' not in data:
+      buf.append(data)
+      return
+    line_end, extra = data.rsplit('\n', 1)
+    self.buf.append(line_end)
+    lines = ''.join(self.buf).split('\n')
+    buf.clear()
+    if extra:
+      self.buf.append(extra)
+    self.callback(lines)
+
+  def writelines(self, lines):
+    return self.write(''.join(lines))
+
+class LogWriter(LineCallbackWriter):
+
+  def __init__(self, log, level='DEBUG', tag=None):
+    super().__init__()
+    self.name = name
+    self.log = getattr(log, level.lower())
+
+  def callback(self, lines):
+    for line in lines:
+      if self.tag:
+        line = f'{tag} {line}'
+      self.log(line)
