84a6369bab6b73b21fbf400bacec0467e910d092
eckso <eckso@eckso.io>
2019-06-15 22:28:10 -0400

    fix imports, add spawn class method

--- lura/threads.py
+++ lura/threads.py
@@ -1,3 +1,4 @@
+import sys
 import threading
 from lura.attrs import attr
 from multiprocessing.pool import ThreadPool
@@ -17,6 +18,12 @@ pool = attr(
 
 class Thread(threading.Thread):
 
+  @classmethod
+  def spawn(cls, *args, **kwargs):
+    thread = cls(*args, **kwargs)
+    thread.start()
+    return thread
+
   def __init__(
     self,
     group=None,
@@ -26,16 +33,20 @@ class Thread(threading.Thread):
     kwargs={},
     *,
     daemon=None,
+    reraise=True,
   ):
-    super().__init__(group=group, name=name, daemon=daemon, target=self.work_)
+    super().__init__(group=group, name=name, daemon=daemon, target=self.work__)
     self.target_ = target
     self.args_ = args
     self.kwargs_ = kwargs
     self.result = None
     self.error = None
+    self.reraise = reraise
 
-  def work_(self):
+  def work__(self):
     try:
       self.result = self.target_(*self.args_, **self.kwargs_)
     except Exception:
       self.error = sys.exc_info()
+      if self.reraise:
+        raise
