fd93909fee5b9eee37f1884dd3cb39adcb7e798b
eckso <eckso@eckso.io>
2019-06-17 23:57:56 -0400

    import contexts directly

--- lura/run/__init__.py
+++ lura/run/__init__.py
@@ -1,11 +1,20 @@
 from .run import run
-from . import context
-run.Context = context.Context
-run.Quash = context.Quash
-run.Enforce = context.Enforce
-run.Cwd = context.Cwd
-run.Stdio = context.Stdio
-run.Log = context.Log
-run.Sudo = context.Sudo
-run.New = context.New
-del context
+from .context import Context
+from .context import Quash
+from .context import Enforce
+from .context import Cwd
+from .context import Shell
+from .context import Stdio
+from .context import Log
+from .context import Sudo
+from .context import New
+
+run.Context = Context
+run.Quash = Quash
+run.Enforce = Enforce
+run.Cwd = Cwd
+run.Shell = Shell
+run.Stdio = Stdio
+run.Log = Log
+run.Sudo = Sudo
+run.New = New
