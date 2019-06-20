65ca5e6b85ff007c1d8301883ed28527e633fd81
eckso <eckso@eckso.io>
2019-06-19 20:08:11 -0400

    improve ConCurl

--- lura/testing/concurl.py
+++ lura/testing/concurl.py
@@ -6,18 +6,7 @@ from collections.abc import Mapping, Sequence
 from lura import logs
 from lura.attrs import attr
 from lura.threads import pool
-
-def Request(endpoint, type, headers, data):
-  return attr(
-    endpoint=endpoint, type=type, headers=headers, data=data)
-
-def Response(headers, code, text, exc_info, start, end):
-  return attr(
-    headers=headers, code=code, text=text, exc_info=exc_info, start=start,
-    end=end)
-
-def Result(id, request, response, start, end):
-  return attr(id=id, request=request, response=response, start=start, end=end)
+from statistics import mean
 
 class ConCurl:
 
@@ -54,6 +43,18 @@ class ConCurl:
     else:
       raise ValueError(f'Type not supported: {request_headers}')
 
+  def new_request(self, endpoint, type, headers, data):
+    return attr(
+      endpoint=endpoint, type=type, headers=headers, data=data)
+
+  def new_response(self, headers, code, text, exc_info, start, end):
+    return attr(
+      headers=headers, code=code, text=text, exc_info=exc_info, start=start,
+      end=end)
+
+  def new_result(self, id, request, response, start, end):
+    return attr(id=id, request=request, response=response, start=start, end=end)
+
   def build_request_endpoint(self, *args, **kwargs):
     return self.endpoint
 
@@ -72,7 +73,7 @@ class ConCurl:
     headers = self.build_request_headers(id=id, endpoint=endpoint, type=type)
     data = self.build_request_data(
       id=id, endpoint=endpoint, type=type, headers=headers)
-    return Request(endpoint, type, headers, data)
+    return self.new_request(endpoint, type, headers, data)
 
   def build_requests(self):
     return ((id, self.build_request(id)) for id in range(self.request_count))
@@ -110,13 +111,50 @@ class ConCurl:
       else:
         c = '?'
       print(c, end='', flush=True)
-    return Response(headers, code, text, exc_info, start, end)
+    return self.new_response(headers, code, text, exc_info, start, end)
 
   def test(self, args):
     start = time.time()
     id, request = args
     response = self.request(request)
-    return Result(id, request, response, start, time.time())
+    return self.new_result(id, request, response, start, time.time())
+
+  def handle_results(self, start, end, results):
+    args = attr()
+    args.start = start
+    args.end = end
+    args.runtime = end - start
+    args.res = results
+    args.res_200 = [
+      res for res in results
+      if res.response.code == 200 and res.response.exc_info is None
+    ]
+    args.res_not_200 = [
+      res for res in results
+      if res.response.code != 200 and res.response.exc_info is None
+    ]
+    args.res_timeouts = [
+      res for res in results
+      if res.response.exc_info is not None and
+        isinstance(res.response.exc_info[1], Timeout)
+    ]
+    args.res_exc = [
+      res for res in results
+      if res.response.exc_info is not None and
+        not isinstance(res.response.exc_info[1], Timeout)
+    ]
+    args.res_bad = args.res_not_200 + args.res_exc
+    args.res_num_succeeded = len(args.res_200)
+    args.res_num_failed = len(args.res_not_200)
+    args.res_num_timeouts = len(args.res_timeouts)
+    args.res_num_raised = len(args.res_exc)
+    args.res_times = [
+      res.response.end - res.response.start for res in args.res_200
+    ]
+    args.time_mean = mean(args.res_times) if args.res_times else 0.0
+    args.time_min = min(args.res_times) if args.res_times else 0.0
+    args.time_max = max(args.res_times) if args.res_times else 0.0
+    return args
 
   def run(self):
     start = time.time()
@@ -127,5 +165,5 @@ class ConCurl:
     results = pool.map(self.thread_count, self.test, self.build_requests())
     end = time.time()
     if self.print_dots:
-      print()
-    return start, time.time(), results
+      print(flush=True)
+    return self.handle_results(start, end, results)
