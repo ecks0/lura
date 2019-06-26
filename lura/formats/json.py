a22af9f3458d836951afa7f2427581ecdfd791bb
eckso <eckso@eckso.io>
2019-06-26 14:22:44 -0400

    simplify and improve lura.formats.json

--- lura/formats/json.py
+++ lura/formats/json.py
@@ -1,61 +1,63 @@
 import json
+from io import StringIO
 from lura.attrs import ottr
 from lura.formats.base import Format
+
+class Encoder(json.JSONEncoder):
+  'Custom json encoder which support sets.'
+
+  def __init__(self, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+
+  def default(self, _):
+    if isinstance(_, set):
+      return list(_)
+    return super().default(_)
 
 class Json(Format):
   '''
   Thin wrapper for json. This class configures the json module to use
-  ordered dictionaries for backing dicts, and indented json. This class
-  supports dumping sets (as lists).
+  ordered dictionaries for backing dicts, and indented json.
   '''
 
-  class Encoder(json.JSONEncoder):
-    'Custom json encoder which support sets.'
-
-    def __init__(self, *args, **kwargs):
-      super().__init__(*args, **kwargs)
-
-    def default(self, _):
-      if isinstance(_, set):
-        return list(_)
-      return super().default(_)
-
-  def __init__(self, indent=4, object_pairs_hook=ottr):
+  def __init__(self):
     super().__init__()
-    self.indent = indent
-    self.object_pairs_hook = object_pairs_hook
+    self.object_pairs_hook = ottr
 
-  def loads(self, data):
-    'Load json from string ``data``.'
+  def loads(self, data, **kwargs):
+    'Load json from string `data`.'
 
-    return json.loads(data, object_pairs_hook=self.object_pairs_hook)
+    kwargs.setdefault('object_pairs_hook', self.object_pairs_hook)
+    return json.loads(data, **kwargs)
 
-  def loadf(self, src, encoding=None):
-    'Load json from file ``src``.'
+  def loadf(self, src, encoding=None, **kwargs):
+    'Load json from file `src`.'
 
     with open(src, encoding=encoding) as fd:
-      return json.load(fd, object_pairs_hook=self.object_pairs_hook)
+      return self.loadfd(fd, **kwargs)
 
-  def loadfd(self, fd):
-    'Load json from file descriptor ``fd``.'
+  def loadfd(self, fd, **kwargs):
+    'Load json from file descriptor `fd`.'
 
-    return self.loads(fd.read()) # FIXME
+    kwargs.setdefault('object_pairs_hook', self.object_pairs_hook)
+    return json.load(fd, **kwargs)
 
-  def dumps(self, data):
-    'Return dict ``data`` as json.'
+  def dumps(self, data, **kwargs):
+    'Return dict `data` as json.'
 
-    return json.dumps(data, indent=self.indent, cls=self.Encoder)
+    kwargs.setdefault('cls', Encoder)
+    return json.dumps(data, **kwargs)
 
-  def dumpf(self, data, dst, encoding=None):
-    'Write dict ``data`` as json to file ``dst``.'
+  def dumpf(self, data, dst, encoding=None, **kwargs):
+    'Write dict `data` as json to file `dst`.'
 
-    data = json.dumps(data, indent=self.indent, cls=self.Encoder)
     with open(dst, 'w', encoding=encoding) as fd:
-      fd.write(data)
+      self.dumpfd(data, fd, **kwargs)
 
-  def dumpfd(self, data, fd):
-    'Write dict ``data`` as json to file descriptor ``fd``.'
+  def dumpfd(self, data, fd, **kwargs):
+    'Write dict `data` as json to file descriptor `fd`.'
 
-    fd.write(self.dumps(data))
+    kwargs.setdefault('cls', Encoder)
+    json.dump(data, fd, **kwargs)
     if hasattr(fd, 'flush') and callable(fd.flush):
       fd.flush()
