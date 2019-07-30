b9d3b7d575c93da2a18f3907887319ce119deb25
eckso <eckso@eckso.io>
2019-07-30 13:40:48 -0400

    refactor lura.formats.base merge functions

--- lura/formats/base.py
+++ lura/formats/base.py
@@ -34,35 +34,11 @@ class Format:
   def dumpfd(self, data, fd):
     pass
 
-  def mergef(self, path, patch, encoding=None):
-    '''
-    Merge data into an existing file.
-
-    :param dict patch: data to merge
-    :param str path: path to file containing data to load and merge with patch
-
-    1. Read ``path`` file contents into data dict
-    2. Merge ``patch`` into data dict
-    3. Write write merged data dict back to ``path``, if needed
-
-    If ``path`` doesn't exist, then ``patch`` is dumped to ``path``.
-    '''
-    if not os.path.isfile(path):
-      return self.dumpf(patch, path, encoding=encoding)
-    data = self.loadf(path)
-    data_hash = hashs(self.dumps(data))
-    merged = merge(patch, data)
-    merged_str = self.dumps(merged)
-    merged_hash = hashs(merged_str)
-    if data_hash == merged_hash:
-      return False
-    with open(path, 'w', encoding=encoding) as fd:
-      write(merged_str)
+  def mergefs(self, path, patch, encoding=None):
+    return merge(self.loadf(path), patch)
 
   def mergeff(self, path, patch, encoding=None):
-    'Merge data from file patch into data at file path.'
-
-    return self.mergef(self.loads(patch), path, encoding=encoding)
+    return merge(self.loadf(path), self.loadf(patch))
 
   def print(self, data, *args, **kwargs):
     print(self.dumps(data).rstrip(), *args, **kwargs)
