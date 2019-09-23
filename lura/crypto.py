efdb75a4f0dd87f8a69c8ed2116e8dac6a0e72ee
eckso <eckso@eckso.io>
2019-09-23 15:06:57 -0400

    update function names in crypto module

--- lura/crypto.py
+++ lura/crypto.py
@@ -2,14 +2,14 @@
 
 from cryptography.fernet import Fernet
 
-def encrypt(data, key=None):
+def encrypts(data, key=None):
   key = Fernet.generate_key() if key is None else key
   fernet = Fernet(key)
   ciphertext = fernet.encrypt(data)
   del fernet
   return (ciphertext, key)
 
-def decrypt(data, key):
+def decrypts(data, key):
   fernet = Fernet(key)
   plaintext = fernet.decrypt(data)
   del fernet, key
