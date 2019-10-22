28c44ceb9e395244893442459f524a2910fd4d22
eckso <eckso@eckso.io>
2019-10-22 00:38:00 -0400

    add net.is_ip_address()

--- lura/net.py
+++ lura/net.py
@@ -1,8 +1,16 @@
+import ipaddress
 import socket
 
-def address(hostname):
+def resolve(hostname):
   try:
     info = socket.getaddrinfo(hostname, 80, proto=socket.IPPROTO_TCP)
     return info[0][4][0]
   except socket.gaierror:
     return None
+
+def is_ip_address(string):
+  try:
+    ipaddress.ip_address(string)
+    return True
+  except ValueError:
+    return False
