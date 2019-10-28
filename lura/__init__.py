c2e4980dae66ede21bb441e901a33551b488df64
eckso <eckso@eckso.io>
2019-10-28 02:49:17 -0400

    update default log level and format

--- lura/__init__.py
+++ lura/__init__.py
@@ -2,7 +2,8 @@ from .log import Logging
 
 logs = Logging(
   std_logger = __name__,
-  std_format = Logging.formats.bare,
+  std_format = Logging.formats.hax,
+  std_level  = Logging.INFO,
 )
 
 del Logging
