a5503a71c55f471949bd0730cbfd8fc1fc83e753
eckso <eckso@eckso.io>
2019-10-28 02:49:41 -0400

    add wget* family of functions to net module

--- lura/net.py
+++ lura/net.py
@@ -1,5 +1,8 @@
+import io
 import ipaddress
+import requests
 import socket
+import sys
 
 def resolve(hostname):
   try:
@@ -14,3 +17,23 @@ def is_ip_address(string):
     return True
   except ValueError:
     return False
+
+def wgetfd(url, fd, bufsize=64*1024):
+  with requests.get(url, stream=True) as req:
+    req.raise_for_status()
+    for buf in req.iter_content(chunk_size=bufsize):
+      if buf:
+        fd.write(buf)
+
+def wgetf(url, path, bufsize=64*1024):
+  with open(path, 'wb') as pathf:
+    self.wgetfd(url, pathf, bufsize)
+
+def wget(url):
+  with io.BytesIO() as buf:
+    wgetfd(url, buf)
+    return buf.getvalue()
+
+def wgets(url, encoding=None):
+  encoding = encoding or sys.getdefaultencoding()
+  return wget(url).decode(encoding)
