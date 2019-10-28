976578bf4e9e42b84925f2c1a5ebd5df14181902
eckso <eckso@eckso.io>
2019-10-28 02:51:03 -0400

    utils.Kwargs no longer tries to be clever

--- lura/utils.py
+++ lura/utils.py
@@ -25,25 +25,11 @@ def deepcopy(obj, map_cls=attr, seq_cls=None):
   else:
     raise ValueError(f'obj is not a MutableMapping or MutableSequence: {obj}')
 
-class KwargError(RuntimeError): pass
-
 class Kwargs:
 
-  def __init__(self, **kwargs):
+  def __init__(self, *args, **kwargs):
     super().__init__()
-    cls = type(self)
-    for arg in kwargs:
-      if arg in self.__dict__.keys():
-        raise KwargError(f'Internal attributes are forbidden: {arg}')
-      if arg[0] == '_':
-        raise KwargError(f'Attributes starting with _ are forbidden: {arg}')
-      if not hasattr(cls, arg):
-        raise KwargError(f'Class {cls.__name__} has no attribute: {arg}')
-      self_attr = getattr(self, arg, None)
-      if callable(self_attr) and not isinstance(self_attr, type):
-        raise KwargError(f'Non-type callable attributes are forbidden: {arg}')
-    for arg in kwargs:
-      setattr(self, arg, kwargs[arg])
+    vars(self).update(kwargs)
 
 def isexc(o):
   '''
