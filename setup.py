0b610bc307b0c1e18f861d6496b00665faef99f2
eckso <eckso@eckso.io>
2019-11-03 21:49:10 -0500

    increment release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 name             = 'lura'
-version          = '0.4.6'
+version          = '0.4.7'
 author           = 'Nick Zigarovich'
 author_email     = 'nick@zigarovich.io'
 url              = 'https://github.com/ecks0/lura'
