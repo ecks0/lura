a8eed3844d22c43761cffe89370edbaf0f1bc97e
eckso <eckso@eckso.io>
2020-05-19 18:57:14 -0400

    update typecheck script

--- typecheck.sh
+++ typecheck.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-null() { "$@" >/dev/null 2>&1; return $?; }
-
-null which mypy || null which pytest || {
-  echo 'Error: neither mypy nor pytype is installed'
-  exit 1
-}
-
 clear
-#null which mypy && mypy lura | grep -vE 'ruamel|#missing-imports'
-null which pytype && pytype lura
+pytype lura|less
+
